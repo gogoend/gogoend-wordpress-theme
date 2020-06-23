0878b3cfc3175f8169c2bdeb8864043d122febcc
gogoend <gogoend@qq.com>
2020-06-23 22:18:33 +0800

    初始化主题构建

---
+++ header.php
@@ -0,0 +1,17 @@
+<?php
+/*
+* @Author: gogoend
+* @Date: 2020-06-23 21:32:23
+ * @LastEditors: gogoend
+ * @LastEditTime: 2020-06-23 21:33:48
+ * @FilePath: \gogoend\header.php
+* @Description:
+*/
+?>
+<header>
+    <h1>
+        <a name="toc-3"></a>
+        <a href="<?php bloginfo('url'); ?>"><?php bloginfo('name'); ?></a>
+    </h1>
+    <?php bloginfo('description'); ?>
+</header>

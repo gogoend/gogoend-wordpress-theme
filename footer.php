a302e1c0862f81579e850ca3ec274125b526f6ce
gogoend <gogoend@qq.com>
2020-06-26 16:28:36 +0800

    enhance: 版权年份

--- footer.php
+++ footer.php
@@ -3,12 +3,12 @@
  * @Author: gogoend
  * @Date: 2020-06-24 00:19:28
  * @LastEditors: gogoend
- * @LastEditTime: 2020-06-26 16:26:16
+ * @LastEditTime: 2020-06-26 16:28:32
  * @FilePath: \gogoend\footer.php
  * @Description: 
  */
 ?>
 <footer>
-    Copyright © 2007 <?php bloginfo('name'); ?><br />
+    Copyright © 2018-2020 <?php bloginfo('name'); ?><br />
     滇ICP备18002771号-1	
 </footer>

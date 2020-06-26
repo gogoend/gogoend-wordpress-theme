f3b7751e942c7cf11f3a2020856a810154a9d3d4
gogoend <gogoend@qq.com>
2020-06-26 16:22:33 +0800

    enhance: 404页面

---
+++ 404.php
@@ -0,0 +1,23 @@
+<?php
+/*
+ * @Author: gogoend
+ * @Date: 2020-06-26 15:54:37
+ * @LastEditors: gogoend
+ * @LastEditTime: 2020-06-26 16:15:24
+ * @FilePath: \gogoend\404.php
+ * @Description: 
+ */ 
+
+get_header();
+?>
+
+<h1>404 Not Found</h1>
+<h2>
+您所访问的页面不存在
+</h2>
+<h3>就像站长没有女朋友
+	<a target="_blank" href="http://wpa.qq.com/msgrd?v=3&uin=915584781&site=qq&menu=yes"><img border="0" src="http://wpa.qq.com/pa?p=2:915584781:47" alt="点击这里给我发消息" title="点击这里给我发消息"/></a>
+</h3>
+
+<?php
+get_footer();
